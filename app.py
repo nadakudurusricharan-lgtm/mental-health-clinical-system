7ab71bc4836e32f7870a740bc2fc24a83a9df089
nadakudurusricharan-lgtm <nadakudurusricharan@gmail.com>
2026-01-03 13:14:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,10 +123,12 @@ def home():
         screen = int(request.form["screen"])
 
         result = doctor_report(age, stress, mood, sleep, study, activity, screen)
+return render_template("index.html", result=result)
 
-    return render_template("index.html", result=result)
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+   
 
